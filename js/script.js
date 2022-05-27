d8fe6f684941ba0790bc8993f7c3f27e23303197
Nastya Semyonova <nastya.semyonova.ss@gmail.com>
2022-05-27 21:15:42 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+    let navLinks= document.querySelector(".navbar");
+    document.querySelector(".menu").addEventListener("click",()=>{
+        navLinks.classList.toggle("show");
+
+    // if(navLinks.classList.contains("show")){
+    //     navLinks.style.display="block";
+    // }else{
+    //     navLinks.style.display="none";
+    // }
+    })
+    
